c74ca81dec80dcf8f4edd392f82faa4c536c3cb1
anamklone <amk226@pitt.edu>
2018-06-19 15:20:58 -0400

    remove imports no longer used

--- android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/ManualControl.java
+++ android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/ManualControl.java
@@ -1,22 +1,11 @@
 package pitt.ece1896.humblehome;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ScrollView;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.StringReader;
-import java.util.List;
 
 public class ManualControl extends Fragment {
 
