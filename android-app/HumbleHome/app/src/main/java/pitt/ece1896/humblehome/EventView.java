410ba817117ee2009e37270702e421905182dbf8
anamklone <amk226@pitt.edu>
2018-07-05 15:53:23 -0400

    add schedule and events; need to fix event formatting and UI for control tab

---
+++ android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/EventView.java
@@ -0,0 +1,98 @@
+package pitt.ece1896.humblehome;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import pitt.ece1896.humblehome.BreakerView.BreakerState;
+
+public class EventView extends TableLayout {
+
+    private LinearLayout layout;
+
+    private TextView switchState;
+    private TextView switchLabel;
+    private TextView switchId;
+    private TextView eventDate;
+    private TextView eventTime;
+
+    private int id;
+    private BreakerView breaker;
+    private BreakerState newState;
+    private Date dateTime;
+
+    public EventView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public EventView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        init(context);
+    }
+
+    private void init(Context context) {
+        String service = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(service);
+        layout = (TableLayout) layoutInflater.inflate(R.layout.event_view, this, true);
+
+        switchState = (TextView) layout.findViewById(R.id.switchState);
+        switchLabel = (TextView) layout.findViewById(R.id.switchLabel);
+        switchId = (TextView) layout.findViewById(R.id.switchId);
+        eventDate = (TextView) layout.findViewById(R.id.eventDate);
+        eventTime = (TextView) layout.findViewById(R.id.eventTime);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BreakerView getBreaker() {
+        return this.breaker;
+    }
+
+    public void setBreaker(BreakerView breaker) {
+        this.breaker = breaker;
+        this.switchLabel.setText(breaker.getLabel());
+        this.switchId.setText("Breaker ID: " + breaker.getId());
+        invalidate();
+        requestLayout();
+    }
+
+    public BreakerState getNewState() {
+        return this.newState;
+    }
+
+    public void setNewState(BreakerState newState) {
+        this.newState = newState;
+        this.switchState.setText(newState.toString());
+        invalidate();
+        requestLayout();
+    }
+
+    public Date getDateTime() {
+        return this.dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG);
+        DateFormat timeFormat = DateFormat.getTimeInstance(DateFormat.SHORT);
+        this.eventDate.setText(dateFormat.format(dateTime));
+        this.eventTime.setText(timeFormat.format(dateTime));
+        invalidate();
+        requestLayout();
+    }
+
+}
--- android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/ScheduleControl.java
+++ android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/ScheduleControl.java
@@ -5,8 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.Date;
 
 public class ScheduleControl extends Fragment {
+
+    public static EventView[] events;
 
     public static ScheduleControl newInstance() {
         ScheduleControl fragment = new ScheduleControl();
@@ -21,7 +26,26 @@ public class ScheduleControl extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.schedule_layout, container, false);
+
+        View scheduleView = inflater.inflate(R.layout.schedule_layout, container, false);
+
+        LinearLayout layout = (LinearLayout) scheduleView.findViewById(R.id.layout);
+
+        BreakerView testBreaker = new BreakerView(getContext());
+        testBreaker.setId(2);
+        testBreaker.setLabel("Test Breaker");
+
+        events = new EventView[5];
+        for (int i = 0; i < events.length; i++) {
+            events[i] = new EventView(getContext());
+            events[i].setId(i);
+            events[i].setBreaker(testBreaker);
+            events[i].setNewState(BreakerView.BreakerState.values()[i % 4]);
+            events[i].setDateTime(new Date());
+            layout.addView(events[i]);
+        }
+
+        return scheduleView;
     }
 
 }
