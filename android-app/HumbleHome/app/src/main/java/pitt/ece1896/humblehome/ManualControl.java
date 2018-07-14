b257bd5b6357663532723720be080c3c125b9e96
anamklone <amk226@pitt.edu>
2018-07-14 11:22:48 -0400

    integrate AWS DynamoDB access; get list of all breakers in breakers table

--- android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/ManualControl.java
+++ android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/ManualControl.java
@@ -1,15 +1,29 @@
 package pitt.ece1896.humblehome;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBQueryExpression;
+import com.amazonaws.models.nosql.BreakersDO;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import pitt.ece1896.humblehome.BreakerView.BreakerState;
+
 public class ManualControl extends Fragment {
 
-    public static BreakerView[] breakers;
+    private static final String TAG = "ManualControl";
+    public static List<BreakerView> breakers = new ArrayList<BreakerView>();
 
     public static ManualControl newInstance() {
         ManualControl fragment = new ManualControl();
@@ -25,22 +39,32 @@ public class ManualControl extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View manualView = inflater.inflate(R.layout.manual_layout, container, false);
+        return inflater.inflate(R.layout.manual_layout, container, false);
 
-        LinearLayout layout = (LinearLayout) manualView.findViewById(R.id.layout);
+        /*LinearLayout layout = (LinearLayout) manualView.findViewById(R.id.layout);
 
-        breakers = new BreakerView[32];
-        for (int i = 0; i < breakers.length; i++) {
-            breakers[i] = new BreakerView(getContext());
-            breakers[i].setId(i);
-            breakers[i].setLabel("Breaker Label");
-            breakers[i].setDescription("Description");
-            breakers[i].setBreakerState(BreakerView.BreakerState.values()[i % 4]);
-            layout.addView(breakers[i]);
-        }
+        Runnable runnable = new Runnable() {
+            public void run() {
+                BreakersDO template = new BreakersDO();
+                template.setUserId(MainActivity.getUserId());
 
-        return manualView;
+                DynamoDBQueryExpression<BreakersDO> queryExpression = new DynamoDBQueryExpression<BreakersDO>()
+                        .withHashKeyValues(template);
 
+                List<BreakersDO> results = MainActivity.getDynamoDBMapper().query(BreakersDO.class, queryExpression);
+                for (BreakersDO result : results) {
+                    Log.d(TAG, result.toString());
+                    BreakerView breaker = new BreakerView(getContext());
+                    breaker.setId((int)Math.round(result.getBreakerId()));
+                    breaker.setLabel(result.getLabel());
+                    breaker.setDescription(result.getDescription());
+                    breaker.setBreakerState(BreakerState.values()[(int)Math.round(result.getState())]);
+                    breakers.add(breaker);
+                    layout.addView(breaker);
+                }
+            }
+        };
+        Thread myThread = new Thread(runnable);
+        myThread.start();*/
     }
-
 }
