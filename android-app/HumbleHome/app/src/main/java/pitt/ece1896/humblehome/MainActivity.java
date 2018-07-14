735c6d4a3c8024355d1585ead195fbea8f4d7c01
anamklone <amk226@pitt.edu>
2018-07-14 14:38:59 -0400

    integrate Eclipse Paho Android Service (for MQTT protocol); currently works using TCP, but not SSL

--- android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/MainActivity.java
+++ android-app/HumbleHome/app/src/main/java/pitt/ece1896/humblehome/MainActivity.java
@@ -6,9 +6,30 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private MqttAndroidClient mqttAndroidClient;
+    private final String MQTT_TAG = "MQTT - ";
+    //private final String serverUri = "ssl://b-f6c789c3-b708-4d73-b004-2a6245bd7c5d-1.mq.us-east-1.amazonaws.com:8883";
+    private final String serverUri = "tcp://b-f6c789c3-b708-4d73-b004-2a6245bd7c5d-1.mq.us-east-1.amazonaws.com:8883";
+    private final String clientId = "android-app";
+    private final String username = "user";
+    private final String password = "humblehome1896";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +63,70 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_layout, BoardControl.newInstance());
         transaction.commit();
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                if (reconnect) {
+                    Log.d(TAG, MQTT_TAG + "Reconnected to: " + serverUri);
+
+                } else {
+                    Log.d(TAG, MQTT_TAG + "Connected to: " + serverUri);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.d(TAG, MQTT_TAG + "Connection lost");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d(TAG, MQTT_TAG + "Message arrived\nTopic: " + topic + "\nPayload: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                try {
+                    Log.d(TAG, MQTT_TAG + "Delivery complete\nMessage: " + new String(token.getMessage().getPayload()));
+                } catch (MqttException ex) {
+                    Log.e(TAG, MQTT_TAG + ex.toString());
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setUserName(username);
+        mqttConnectOptions.setPassword(password.toCharArray());
+
+        try {
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d(TAG, MQTT_TAG + "Failed to connect to: " + serverUri + "\nException: " + exception.toString());
+                }
+            });
+        } catch (MqttException ex) {
+            Log.e(TAG, MQTT_TAG + ex.toString());
+            ex.printStackTrace();
+        }
     }
 
+    public MqttAndroidClient getMqttAndroidClient() {
+        return this.mqttAndroidClient;
+    }
 }
